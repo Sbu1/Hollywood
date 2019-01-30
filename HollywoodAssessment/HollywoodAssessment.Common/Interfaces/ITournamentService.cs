772c68faefb5a828862289526f12f4fb7ae6714a
Sbu1 <sbuddaz@gmail.com>
2019-01-30 23:54:30 +0200

    feat, added services

---
+++ HollywoodAssessment/HollywoodAssessment.Common/Interfaces/ITournamentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HollywoodAssessment.Data.Models;
+
+namespace HollywoodAssessment.Common.Interfaces
+{
+  public interface ITournamentService
+  {
+    void CreateTournament(Tournament tournament);
+    void UpdateTournament(int id,Tournament tournament);
+    void DeleteTournament(int id,Tournament tournament);
+  }
+}
---
+++ HollywoodAssessment/HollywoodAssessment.Service/Service/TournamentService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HollywoodAssessment.Common.Interfaces;
+using HollywoodAssessment.Data.Models;
+
+namespace HollywoodAssessment.Service.Service
+{
+  public class TournamentService : ITournamentService
+  {
+    public HollywoodAssessmentDbContext db;
+
+    public TournamentService(HollywoodAssessmentDbContext hollywoodAssessmentDb)
+    {
+      db = hollywoodAssessmentDb;
+    }
+    public void CreateTournament(Tournament tournament)
+    {
+      db.Tournament.Add(tournament);
+      db.SaveChanges();
+    }
+
+    public void UpdateTournament(int id, Tournament tournament)
+    {
+      var result = GetTournament(id);
+      result.TournamentName = tournament.TournamentName;
+      db.SaveChanges();
+    }
+
+    public void DeleteTournament(int id)
+    {
+      var result = GetTournament(id);
+      db.Remove(result);
+    }
+
+    public Tournament GetTournament(int id)
+    {
+      var result = db.Tournament.FirstOrDefault(x => x.TournamentId == id);
+      //Throw exception for null
+
+      return result;
+    }
+  }
+}
