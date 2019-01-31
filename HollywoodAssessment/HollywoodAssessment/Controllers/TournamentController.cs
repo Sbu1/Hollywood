cc3cf9c6eaf41f494f520aee6d5be052993281b9
Sbu1 <sbuddaz@gmail.com>
2019-01-31 18:32:51 +0200

    feat, added redirect to event action method for tournament

--- HollywoodAssessment/HollywoodAssessment/Controllers/TournamentController.cs
+++ HollywoodAssessment/HollywoodAssessment/Controllers/TournamentController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HollywoodAssessment.Common.Interfaces;
+using HollywoodAssessment.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -9,38 +11,45 @@ using Microsoft.AspNetCore.Mvc;
 namespace HollywoodAssessmen.API.Controllers
 {
   [Route("api/[controller]")]
-  public class Tournament : Controller
+  public class TournamentController : Controller
   {
-    // GET: api/<controller>
-    [HttpGet]
-    public IEnumerable<string> Get()
+    private readonly ITournamentService _tournamentService;
+
+    public TournamentController(ITournamentService tournamentService)
     {
-      return new string[] { "value1", "value2" };
+      _tournamentService = tournamentService;
     }
 
-    // GET api/<controller>/5
+    // GET: api/<controller>
     [HttpGet("{id}")]
-    public string Get(int id)
+    public ActionResult<Tournament> Get(int id)
     {
-      return "value";
+      var result = _tournamentService.GetTournament(id);
+      return result;
     }
 
     // POST api/<controller>
     [HttpPost]
-    public void Post([FromBody]string value)
+    public ActionResult Post([FromBody]Tournament tournament)
     {
+      _tournamentService.CreateTournament(tournament);
+      return RedirectToAction("Post", "Event", new Event() {TournamentId = tournament.TournamentId});
     }
 
     // PUT api/<controller>/5
     [HttpPut("{id}")]
-    public void Put(int id, [FromBody]string value)
+    public ActionResult Put(int id, [FromBody]Tournament tournament)
     {
+      _tournamentService.UpdateTournament(id, tournament);
+      return Ok();
     }
 
     // DELETE api/<controller>/5
     [HttpDelete("{id}")]
-    public void Delete(int id)
+    public ActionResult Delete(int id)
     {
+      _tournamentService.DeleteTournament(id);
+      return NoContent();
     }
   }
 }
